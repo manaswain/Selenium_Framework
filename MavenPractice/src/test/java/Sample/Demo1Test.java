194678f7fbcef6ff8862038299d61f51b2bdc066
manaswain <104189343+manaswain@users.noreply.github.com>
2023-08-24 00:43:41 +0530

    Update Demo1Test.java

--- MavenPractice/src/test/java/Sample/Demo1Test.java
+++ MavenPractice/src/test/java/Sample/Demo1Test.java
@@ -3,18 +3,14 @@ package Sample;
 import org.testng.annotations.Test;
 @Test
 public class Demo1Test {
-		
-	
+			
 	public void m1()
 	{
 		System.out.println("hii");
-	}
-	
+	}	
 	public void m2()
 	{
-		System.out.println("hello");
-		
+		System.out.println("hello");		
 	}
-
 }
-
+//pull back
